23cb8bfd73800a8836015338303869d39c87ae6d
Ivy Qin <yiyiq0709@gmail.com>
2015-06-02 23:41:09 -0700

    Create CostFunction.m

---
+++ CostFunction.m
@@ -0,0 +1,14 @@
+function cost=CostFunction(X,Y,theta)
+m=size(X,1);
+% m is the number of samples
+cost=0;
+for k=1:+1:m
+    X_test=sigmoid(X(k,:)*theta);
+    if (Y(k,1)==1)
+        cost=cost+(-log(X_test));
+    else
+        cost=cost+(-log(1-X_test));
+    end
+end
+ cost=cost/(m*1.0 );  
+end
