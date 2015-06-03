ce3668873de4c7d3137e0067a3afccd9e246be6c
Ivy Qin <yiyiq0709@gmail.com>
2015-06-02 23:42:59 -0700

    Create sigmoid.m

---
+++ sigmoid.m
@@ -0,0 +1,17 @@
+function g = sigmoid(z)
+%SIGMOID Compute sigmoid function
+
+%g = 1./(1 + e.^(-z));
+
+%SIGMOID Compute sigmoid function
+%   J = SIGMOID(z) computes the sigmoid of z.
+
+% You need to return the following variables correctly 
+g = zeros(size(z));
+
+% Compute the sigmoid of each value of z (where z can be a matrix,
+% vector or scalar).
+
+g = 1./(1 + exp(-z));
+
+end
